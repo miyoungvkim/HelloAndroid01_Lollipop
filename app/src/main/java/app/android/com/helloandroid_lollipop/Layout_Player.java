82e05b1ad68068189132d3633a7bc1841cf6406a
miyoungvkim <miyoungvkim@gmail.com>
2018-04-10 11:16:12 +0900

    add youtube player view

--- app/src/main/java/app/android/com/helloandroid_lollipop/Layout_Player.java
+++ app/src/main/java/app/android/com/helloandroid_lollipop/Layout_Player.java
@@ -7,14 +7,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 /**
  * Created by kjphoto on 2018-04-09.
  */
 
-public class Layout_Player extends AppCompatActivity {
+public class Layout_Player extends YouTubeBaseActivity {
 
-    private Button btn;
-
+    YouTubePlayerView youtubeView;
+    Button button;
+    YouTubePlayer.OnInitializedListener listener;
     private static final String TAG = "TAG";
 
     @Override
@@ -24,15 +30,29 @@ public class Layout_Player extends AppCompatActivity {
 
         Log.d(TAG," "+new Throwable().getStackTrace()[0].getMethodName()+"()"+"#"+new Throwable().getStackTrace()[0].getLineNumber());
 
-        //버튼에 대한 참조
-        btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener(){
+        button = (Button) findViewById(R.id.youtubeButton);
+        youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+        listener = new YouTubePlayer.OnInitializedListener() {
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d(TAG," "+new Throwable().getStackTrace()[0].getMethodName()+"()"+"#"+new Throwable().getStackTrace()[0].getLineNumber());
+                youTubePlayer.loadVideo("UPRuZcaZs9o"); // load 및 재생할 youtube id 입력
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+                Log.d(TAG," "+new Throwable().getStackTrace()[0].getMethodName()+"()"+"#"+new Throwable().getStackTrace()[0].getLineNumber());
+            }
+        };
+
+        button.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Layout_Service.class);
-                startActivity(intent);
+                Log.d(TAG," "+new Throwable().getStackTrace()[0].getMethodName()+"()"+"#"+new Throwable().getStackTrace()[0].getLineNumber());
+                youtubeView.initialize("AIzaSyADPz8ugG7yUQh__aOubcEN-yf-zkYjiXs\t", listener);
             }
-        });
+        } );
     }
-
 }
